eb076187a26426188ac89ccd4bf58876f75a1f21
Winnie Kelley <w642kelley@gmail.com>
2025-01-21 23:21:05 -0600

    Finished Phase 4, Step 2. Adding a check mark as an element to a list

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ newIngredientElement.textContent = 'Jalapenos';
 
 // And finally, let's add that list item as a child of the ul.
 document.querySelector('#ingredientsList').appendChild(newIngredientElement);
+
+
+// Phase 4 Step 2: Adding a check mark as an element to a list
+let myDiv = document.getElementById("ingredientsList");
+
+// create checkbox element
+let checkbox = document.createElement("input");
+
+// add the attributes for a checkbox
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+
+// create a label for the checkbox
+let label = document.createElement("label");
+
+// assign the attributes to the label tag
+label.htmlFor = "id";
+
+//append it
+label.appendChild(document.createTextNode("This create the label for checkbox"));
+
+myDiv.appendChild(checkbox);
+myDiv.appendChild(label);
