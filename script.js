97508972c5a3adeed62edd1bab6d3d806c9916a8
Winnie Kelley <w642kelley@gmail.com>
2025-01-23 14:11:19 -0600

    Finished Phase 4, step 4. Adding strikethrough when chekbox is checked.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ newIngredientElement.textContent = 'Jalapenos';
 // And finally, let's add that list item as a child of the ul.
 document.querySelector('#ingredientsList').appendChild(newIngredientElement);
 
-
+/*-------------------------------------------------*/
 // Phase 4 Step 2: Adding a check mark as an element to a list
 let myDiv = document.getElementById("ingredientsList");
 
@@ -17,13 +17,12 @@ let checkbox = document.createElement("input");
 
 // add the attributes for a checkbox
 checkbox.type = "checkbox";
-checkbox.name = "name";
-checkbox.value = "value";
-checkbox.id = "id";
+// checkbox.name = "name";
+// checkbox.value = "value";
+// checkbox.id = "id";
 
 // create a label for the checkbox
 let label = document.createElement("label");
-label.className = "checked";
 
 // assign the attributes to the label tag
 label.htmlFor = "id";
@@ -33,3 +32,19 @@ label.appendChild(document.createTextNode("This create the label for checkbox"))
 
 myDiv.appendChild(checkbox);
 myDiv.appendChild(label);
+
+// Phase 4, step 3/4 add event listener for checking toggle between crossed out and not 
+console.log("Look here",checkbox.checked);
+
+checkbox.addEventListener("click", function(){
+    console.log("here",checkbox.checked);
+
+    if(checkbox.checked){
+        //add checked to class name so it can cross it out
+        console.log("it's checked off");
+        label.classList = "checked";
+    } else{
+        console.log("not checked");
+        label.classList.remove("checked");
+    }
+});
