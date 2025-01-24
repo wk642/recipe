1e6326761873f351e343804899da73a47a007956
Winnie Kelley <w642kelley@gmail.com>
2025-01-23 23:37:08 -0600

    Cleaned up HTML. Thanks Kaylah

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Add new ingredient li
 let newIngredientElement = document.createElement('li');
 newIngredientElement.textContent = 'Jalapenos';
-document.querySelector('#ingredientsList').appendChild(newIngredientElement);
-let myDiv = document.getElementById("ingredientsList");
+document.querySelectorAll('ul')[1].appendChild(newIngredientElement);
 
 // Create checkbox
+let ulIngredients = document.getElementById("ingredientsList");
 let checkbox = document.createElement("input");
 checkbox.type = "checkbox";
 checkbox.name = "name";
 checkbox.value = "value";
 checkbox.id = "id";
-myDiv.appendChild(checkbox);
+ulIngredients.appendChild(checkbox);
 
 // Create label for checkbox
 let label = document.createElement("label");
 label.htmlFor = "id";
 label.appendChild(document.createTextNode("This create the label for checkbox"));
-myDiv.appendChild(label);
+ulIngredients.appendChild(label);
 
 // Add eventlistener to strikethrough checked text
 checkbox.addEventListener("click", function() {
